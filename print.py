611750b46701c562db2ae8a4e10fa65d6752c086
grvenkat40 <grvenkat4562gmail.com>
2024-10-18 20:14:32 +0530

    errors are solved

--- print.py
+++ print.py
@@ -2,12 +2,12 @@ import pandas as pd
 import numpy as np
 def array():
   a=np.random.randint(100,size=5)
-  print(a)
+  return a
 def data():
-data={'Cars':['BMW,'FORD,'TATA','Bugatti','TESLA'],'Price':[$2,00,000,$05,00,000,$3,00,000,$10,00,000,$20,00,000]}
-display=pd.DataFrame(data)
-print(display)
+  data={'Cars':['BMW','FORD','TATA','Bugatti','TESLA'],'Price':['$2,00,000','$05,00,000','$3,00,000','$10,00,000','$20,00,000']}
+  display=pd.DataFrame(data)
+  return display
 list_number=array()
 table_list=data()
 print(list_number)
-print(talble_list)
+print(table_list)
