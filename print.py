63464baa60ec8f8a0c0fbe0fad655f0b582b4ba2
G R Venkat <166822620+grvenkat40@users.noreply.github.com>
2024-10-18 20:04:41 +0530

    Create print.py

---
+++ print.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+def array():
+  a=np.random.randint(100,size=5)
+  print(a)
+def data():
+data={'Cars':['BMW,'FORD,'TATA','Bugatti','TESLA'],'Price':[$2,00,000,$05,00,000,$3,00,000,$10,00,000,$20,00,000]}
+display=pd.DataFrame(data)
+print(display)
+list_number=array()
+table_list=data()
+print(list_number)
+print(talble_list)
